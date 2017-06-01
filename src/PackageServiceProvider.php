05600ab7915ceaab5b4678bae6458da24fe1c290
Zapster <thecodingbeast@gmail.com>
2017-06-01 17:16:41 +0000

    Bootstrap code

---
+++ src/PackageServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ZapsterStudios\CrudGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
